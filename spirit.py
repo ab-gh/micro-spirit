91dc9304ca4bc2e34c72881766afd9510bd188a3
electric-blue-green <thomasbass2001@gmail.com>
2017-09-27 15:27:09 +0100

    displays placeholder when tilt is between 100

--- spirit.py
+++ spirit.py
@@ -1,10 +1,25 @@
 from microbit import *
-
+## /
+r_roll = Image("00009:"
+               "00090:"
+               "00900:"
+               "09000:"
+               "90000")
+##-
+c_roll = Image("00000:"
+               "00000:"
+               "99999:"
+               "00000:"
+               "00000")
+##\
+l_roll = Image("90000:"
+               "09000:"
+               "00900:"
+               "00090:"
+               "00009")
 while True:
-    reading = accelerometer.get_x()
-    if reading > 20:
-        display.show("R")
-    elif reading < -20:
-        display.show("L")
+    roll = accelerometer.get_x()
+    if -100 <= roll <= 100:
+        display.show(r_roll)
     else:
-        display.show("-")
+        display.show(" ")
