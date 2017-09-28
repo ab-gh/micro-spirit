138787d1636e5e2be4c0e162d26e4ba2dbdec4f1
electric-blue-green <thomasbass2001@gmail.com>
2017-09-28 09:01:13 +0100

    *whoops*

--- spirit.py
+++ spirit.py
@@ -17,16 +17,48 @@ l_roll = Image("90000:"
                "00900:"
                "00090:"
                "00009")
+
+l_h_roll = Image("00000:"
+                 "90000:"
+                 "09990:"
+                 "00009:"
+                 "00000")
+
+r_h_roll = Image("00000:"
+                 "00009:"
+                 "09990:"
+                 "90000:"
+                 "00000")
+
+h_roll = Image("00900:"
+               "00900:"
+               "00900:"
+               "00900:"
+               "00900")
+
+if button_a.is_pressed():
+    button = "a"
+else:
+     button = "b"
+
 while True:
     roll = accelerometer.get_x()
     if -100 <= roll <= 100:
         display.show(c_roll)
-    elif roll > 100:
+    elif 100 <= roll <= 500:
+        display.show(r_h_roll)
+    elif -500 <= roll <= 100:
+        display.show(l_h_roll)
+    elif 500 <= roll <= 1000:
         display.show(r_roll)
-    else:
+    elif -1000 <= roll <= -500:
         display.show(l_roll)
+    else:
+        display.show(h_roll)
+
 while True:
     roll = accelerometer.get_x()
     if -10 <= roll <= 10:
         display.show(Image.YES)
 
+#flat = -1024
