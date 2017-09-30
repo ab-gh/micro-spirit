08bf42a3d95c37aedc416ae91590a248f7fb6a12
electric-blue-green <thomasbass2001@gmail.com>
2017-09-30 20:12:54 +0100

    i failed with indentation

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from microbit import *
 while True:
     roll = accelerometer.get_x()
     pitch = accelerometer.get_y()
-    if 500 <= pitch <= 600:
+    if -50 <= pitch <= 50:
         display.show(Image.HEART)
     else:
         display.show("x")
